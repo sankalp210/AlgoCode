61d88cd43b51d888b1e5c4496b0e28fc9440525b
Sankalp <sankalpmeshram2003@gmail.com>
2025-07-07 11:52:47 +0000

    Added basic ping check route in fastify

--- AlgoCode-Submission-Service/src/app.js
+++ AlgoCode-Submission-Service/src/app.js
@@ -1,9 +1,17 @@
 const fastifyPlugin = require('fastify-plugin');
 const cors = require('@fastify/cors');
+const servicePlugin = require('./services/servicePlugin');
 
 async function app(fastify, opts) {
     
     await fastify.register(cors);
+
+     fastify.register(servicePlugin);
+
+
+    // register test routes
+    fastify.register(require('./routes/api/apiRoutes'), {prefix: '/api'});
+
 
 }
 
