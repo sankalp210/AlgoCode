8dc497695c202ed91677069cec26c88a66c9c4ab
Sankalp <sankalpmeshram2003@gmail.com>
2025-07-08 12:58:07 +0000

    connected to db

--- AlgoCode-Submission-Service/src/index.js
+++ AlgoCode-Submission-Service/src/index.js
@@ -3,13 +3,15 @@ const fastify = require('fastify')({ logger: false });
 const serverConfig = require('./config/serverConfig');
 
 const app = require('./app');
+const connectToDB = require('./config/dbConfig');
 
 fastify.register(app);
 
-fastify.listen({ port: serverConfig.PORT }, (err) => {
+fastify.listen({ port: serverConfig.PORT }, async (err) => {
     if (err) {
         console.error("❌ Error during server.listen:", err);
         process.exit(1);
     }
+    await connectToDB();
     console.log(`✅ Server is running on port ${serverConfig.PORT}`);
 });
